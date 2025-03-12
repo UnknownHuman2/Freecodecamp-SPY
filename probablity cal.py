f4a0cfb4159762f37159f42e64b014324ac72da5
UnknownHuman2 <155294530+UnknownHuman2@users.noreply.github.com>
2025-03-12 21:07:30 +0530

    Create probablity cal.py

---
+++ probablity cal.py
@@ -0,0 +1,38 @@
+import random
+import copy
+
+class Hat:
+    def __init__(self, **kwargs):
+        self.contents = []
+        for color, count in kwargs.items():
+            self.contents.extend([color] * count)
+
+    def draw(self, num_balls):
+        num_balls = min(num_balls, len(self.contents))
+        drawn_balls = random.sample(self.contents, num_balls)
+        for ball in drawn_balls:
+            self.contents.remove(ball)
+        return drawn_balls
+
+def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
+    successful_experiments = 0
+    for _ in range(num_experiments):
+        hat_copy = copy.deepcopy(hat)
+        drawn_balls = hat_copy.draw(num_balls_drawn)
+        drawn_counts = {}
+        for ball in drawn_balls:
+            drawn_counts[ball] = drawn_counts.get(ball, 0) + 1
+
+        success = True
+        for color, count in expected_balls.items():
+            if drawn_counts.get(color, 0) < count:
+                success = False
+                break
+
+        if success:
+            successful_experiments += 1
+
+        hat_copy.contents = hat.contents[:]
+
+    return successful_experiments / num_experiments
+
